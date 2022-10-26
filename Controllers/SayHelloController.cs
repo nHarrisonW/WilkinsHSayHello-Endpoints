4c932bf0f78dc861762f5b5fd347a213d2a50807
Harrison <wilkinsnigel2000@gmail.com>
2022-10-26 12:56:57 -0700

    FINISHED

--- Controllers/SayHelloController.cs
+++ Controllers/SayHelloController.cs
@@ -1,3 +1,10 @@
+//N. Harrison Wilkins
+//10-26-2022
+//SAY HELLO - ENDPOINTS
+//THIS CONTROLLER WILL CONTROL ALL ENDPOINTS WITHIN THE PROJECT,  WILL WORK WITH WEB API'S
+//PEER REVIEW BY : 
+
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace WilkinsHSayHello_Endpoints.Controllers;
